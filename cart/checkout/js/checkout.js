cc7ddb9fab7702306b20d2d489d709c78ffd72df
kristiansantos11 <kristian.santos2000@gmail.com>
2022-02-12 21:06:31 +0800

    Checkout now halfway done

---
+++ cart/checkout/js/checkout.js
@@ -0,0 +1,7 @@
+var name = document.getElementById('customer_name');
+var contact_number = document.getElementById('contact_number');
+var address = document.getElementById('address');
+
+function checkout_pressed() {
+    
+}
